e8b2333ff4301378a9a107341692c6bdb5a61331
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2024-05-20 09:46:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pickle
 with open('Ferment_linear_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-
 # Define function to make predictions
 def predict(input_data):
     # Preprocess input_data if necessary
@@ -18,14 +17,14 @@ def main():
     st.title('Machine Learning Model Predictor')
 
     # Input features
-    feature1 = st.text_input('WET BIOMASS g', min_value=0, max_value=100)
-    feature2 = st.text_input('DRY BIOMASS(g)', min_value=0, max_value=100)
-    feature3 = st.text_input('CARBON SOURCE', min_value=0, max_value=100)
+    feature1 = st.number_input('WET BIOMASS(g)', min_value=0, max_value=100, step=1)
+    feature2 = st.number_input('DRY BIOMASS(g)', min_value=0, max_value=100, step=1)
+    feature3 = st.text_input('CARBON SOURCE')  # Assuming CARBON SOURCE is a text input
 
     # Button to trigger prediction
     if st.button('Predict'):
         # Make prediction
-        input_data = [[feature1, feature2,feature3]]  # Adjust according to your model's input format
+        input_data = [[feature1, feature2, feature3]]  # Adjust according to your model's input format
         prediction = predict(input_data)
 
         # Display prediction result
@@ -33,4 +32,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
     
